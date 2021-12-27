e8b441c6a9da9951c3d904be2053691edfb510f6
inderjeet <inderjeetofc@gmail.com>
2021-12-27 18:56:09 +0530

    stack with ll and parenthesis problem

--- DSA/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-     printf("\nStack has parenthesis left to be balanced ! Expression is unbalanced");
-    }
-    else if (arrPtr[i] == '\0' && ptr->top <= -1 && count != 0)
-        printf("\nStack has %d ) parenthesis left to be balanced ! Expression is unbalanced", -count);
-        // printf("\nStack has parenthesis left to be balanced ! Expression is unbalanced");
