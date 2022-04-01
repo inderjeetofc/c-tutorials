689a0e9c6bd83dd94c0cb7caf43ec3b3317aa520
inderjeet kaur <inderjeet@unoindia.co>
2022-04-01 15:48:30 +0530

    stack done before prefix and postfix

--- DSA/gfg/stack.cpp
+++ DSA/gfg/stack.cpp
@@ -258,34 +258,230 @@
 // }
 
 // q parenthesis prob with STL;
-#include <bits/stdc++.h>
-using namespace std;
-bool parenthesisCheck(string str)
-{
-    stack<char> s;
-    for (char i : str)
-    {
-        if (i == '[' || i == '{' || i == '(')
-            s.push(i);
-        else
-        {
-            if (s.size() == 0)
-                return 0;
-            if (i == ')' && s.top() == '(')
-                s.pop();
-            else if (i == ']' && s.top() == '[')
-                s.pop();
-            else if (i == '}' && s.top() == '{')
-                s.pop();
-            else
-                return 0;
-        }
-    }
-    return s.empty();
-}
-int main()
-{
-    string str = "[]{(]}]";
-    parenthesisCheck(str) ? cout << "Balanced!" << endl : cout << "Not Balanced!" << endl;
-    return 0;
-}
+// #include <bits/stdc++.h>
+// using namespace std;
+// bool parenthesisCheck(string str)
+// {
+//     stack<char> s;
+//     for (char i : str)
+//     {
+//         if (i == '[' || i == '{' || i == '(')
+//             s.push(i);
+//         else
+//         {
+//             if (s.size() == 0)
+//                 return 0;
+//             if (i == ')' && s.top() == '(')
+//                 s.pop();
+//             else if (i == ']' && s.top() == '[')
+//                 s.pop();
+//             else if (i == '}' && s.top() == '{')
+//                 s.pop();
+//             else
+//                 return 0;
+//         }
+//     }
+//     return s.empty();
+// }
+// int main()
+// {
+//     string str = "[]{(]}]";
+//     parenthesisCheck(str) ? cout << "Balanced!" << endl : cout << "Not Balanced!" << endl;
+//     return 0;
+// }
+
+// q - stock span problem
+// #include <bits/stdc++.h>
+// using namespace std;
+// void stockSpan(int arr[], int n)
+// {
+//     int span;
+//     stack<int> s;
+//     s.push(0);
+//     cout << 1 << " ";
+//     for (int i = 1; i < n; i++)
+//     {
+//         while (!s.empty() && arr[s.top()] <= arr[i])
+//             s.pop();
+//         span = s.empty() ? i + 1 : i - s.top();
+//         s.push(i);
+//         cout << span << " ";
+//     }
+// }
+// int main()
+// {
+//     int arr[] = {60, 10, 20, 40, 35, 38, 50, 70, 65};
+//     stockSpan(arr, 9);
+//     return 0;
+// }
+
+// q- prev greater element
+// #include <bits/stdc++.h>
+// using namespace std;
+// // big O(N) solution
+// void prevGre(int arr[], int n)
+// {
+//     stack<int> s;
+//     s.push(arr[0]);
+//     cout << -1 << " ";
+//     for (int i = 1; i < n; i++)
+//     {
+//         while (!s.empty() && s.top() <= arr[i])
+//             s.pop();
+//         int prev = s.empty() ? -1 : s.top();
+//         s.push(arr[i]);
+//         cout << prev << " ";
+//     }
+// }
+// int main()
+// {
+//     int arr[] = {15, 10, 18, 12, 4, 6, 2, 8};
+//     prevGre(arr, 8);
+//     return 0;
+// }
+// q-next greater element . above prob
+// #include <bits/stdc++.h>
+// using namespace std;
+// // big O(N) solution
+// void nextGre(int arr[], int n)
+// {
+//     stack<int> s;
+//     s.push(arr[n - 1]);
+//     cout << -1 << " ";
+//     for (int i = n - 2; i >= 0; i--)
+//     {
+//         while (!s.empty() && s.top() <= arr[i])
+//             s.pop();
+//         int next = s.empty() ? -1 : s.top();
+//         s.push(arr[i]);
+//         cout << next << " ";
+//     }
+// }
+// int main()
+// {
+//     int arr[] = {5, 15, 10, 8, 6, 12, 9, 18};
+//     nextGre(arr, 8);
+//     return 0;
+// }
+
+// q- modify stack to get the min element also in O(1) aux space;
+// sol-
+// #include <bits/stdc++.h>
+// using namespace std;
+// // method-1 assuming all positive elements to be pushed
+// struct myStack
+// {
+//     int min;
+//     int *arr;
+//     int top;
+//     int capacity;
+//     myStack(int cap)
+//     {
+//         capacity = cap;
+//         arr = new int(cap);
+//         top = -1;
+//         min = INT_MAX;
+//     }
+//     bool isEmpty()
+//     {
+//         return top == -1;
+//     }
+
+//     // ----method-1 assuming all positive elements to be pushed
+
+//     // void push(int data)
+//     // {
+//     //     top++;
+//     //     if (isEmpty())
+//     //     {
+//     //         min = data;
+//     //         arr[top] = data;
+//     //     }
+//     //     else
+//     //     {
+//     //         if (data <= min)
+//     //         {
+//     //             arr[top] = data - min;
+//     //             min = data;
+//     //         }
+//     //         else
+//     //             arr[top] = data;
+//     //     }
+//     // }
+//     // int pop()
+//     // {
+//     //     if (isEmpty())
+//     //         return 0;
+//     //     else
+//     //     {
+//     //         int temp = arr[top];
+//     //         if (arr[top] <= 0)
+//     //         {
+//     //             temp = min;
+//     //             min = min - arr[top];
+//     //         }
+//     //         top--;
+//     //         return temp;
+//     //     }
+//     // }
+
+//     // method-2 assuming when negative elements are also there
+//     // mathematics lga hai thoda sa.
+//     void push(int data)
+//     {
+//         top++;
+//         if (isEmpty())
+//         {
+//             arr[top] = data;
+//         }
+//         else
+//         {
+//             if (data <= min)
+//             {
+//                 arr[top] = 2 * data - min;
+//                 min = data;
+//             }
+//             else
+//                 arr[top] = data;
+//         }
+//     }
+//     int pop()
+//     {
+//         if (isEmpty())
+//             return 0;
+//         else
+//         {
+//             int temp = arr[top];
+//             if (arr[top] <= min)
+//             {
+//                 temp = min;
+//                 min = 2 * min - arr[top];
+//             }
+//             top--;
+//             return temp;
+//         }
+//     }
+//     int getMin()
+//     {
+//         return min;
+//     }
+// };
+// int main()
+// {
+//     myStack s1(5);
+//     s1.push(5);
+//     s1.push(-10);
+//     s1.push(2);
+//     s1.push(-20);
+//     s1.push(1);
+//     cout << "Min = " << s1.getMin() << endl;
+//     cout << s1.pop() << endl;
+//     cout << s1.pop() << endl;
+//     cout << "Min = " << s1.getMin() << endl;
+//     cout << s1.pop() << endl;
+//     cout << s1.pop() << endl;
+//     cout << "Min = " << s1.getMin() << endl;
+//     cout << s1.pop() << endl;
+//     cout << "Min = " << s1.getMin() << endl;
+//     return 0;
+// }
